8f5e53cff10e8846090e12ba0c682c318203c9f6
Nikos Fotiou <nikosft@gmail.com>
2025-05-28 19:04:55 +0300

    controller support

--- edge/relay.sh
+++ edge/relay.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-INPUT="rtp://192.168.154.10:8000"
+INPUT="rtp://192.168.154.10:5000"
 OUTPUT="udp://192.168.154.10:1234?pkt_size=1316"
 
 ~/bin/ffmpeg -i ${INPUT} -c copy -f mpegts ${OUTPUT}
+
+#vlc rtsp://192.168.154.10:8554/camera
