ff2607f78a3143864b043ef382a315d2149456ee
Nikos Fotiou <nikosft@gmail.com>
2025-05-28 16:44:46 +0300

    mediamtx added

--- edge/relay.sh
+++ edge/relay.sh
@@ -3,4 +3,4 @@
 INPUT="rtp://192.168.154.10:8000"
 OUTPUT="udp://192.168.154.10:1234?pkt_size=1316"
 
-~/bin/ffmpeg ffmpeg -i ${INPUT} -c copy -f mpegts ${OUTPUT}
+~/bin/ffmpeg -i ${INPUT} -c copy -f mpegts ${OUTPUT}
